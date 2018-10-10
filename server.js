e311b50711f171902ade79c2e3f91ee6c57d75ac
sonu <sonusaikishan@gmail.com>
2018-10-10 23:40:09 +0530

    Added Node, React & Redux

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 8000;
+
+// Serve static assets
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+// pass all other routes to React that will be handled by React Router
+app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}!`);
+});
